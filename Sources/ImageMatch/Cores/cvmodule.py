99975701a577fcd520b6480f7cd3da416e1fe8b7
NSWell <paris3@163.com>
2021-01-22 18:48:17 +0800

    add comment

--- Sources/ImageMatch/Cores/cvmodule.py
+++ Sources/ImageMatch/Cores/cvmodule.py
@@ -11,7 +11,6 @@ search_params = dict(checks=150)
 flann = cv2.FlannBasedMatcher(index_params, search_params)
 
 
-
 class CVModule():
 
     def __init__(self):
@@ -21,8 +20,7 @@ class CVModule():
         它在空间尺度中寻找极值点，并提取出其位置、尺度、旋转不变数，此算法由David Lowe 在1999年所发表，2004年完善总结。
         @FlannBasedMatcher：Fast Library forApproximate Nearest Neighbors
         """
-   
-        print("CV INIT")
+        pass
 
     def read_base64(self, base64Image):
         """
@@ -31,6 +29,7 @@ class CVModule():
         """
         npimg = np.frombuffer(base64.b64decode(base64Image), dtype=np.uint8)
         img = cv2.imdecode(npimg, 2)
+
         # Enhanced gray
         img = np.uint8(np.clip((2 * (np.int16(img)-60) - 225), 0, 255))
         return img
@@ -59,7 +58,7 @@ class CVModule():
         抽出图像的描述子
         @img:需要抽出描述子的图像
         """
-        img = cv2.resize(img, dsize=shape, interpolation=cv2.INTER_LINEAR)        
+        img = cv2.resize(img, dsize=shape, interpolation=cv2.INTER_LINEAR)
         kps, des = sift.detectAndCompute(img, None)
         return kps, des
 
@@ -75,7 +74,6 @@ class CVModule():
         mid_x, mid_y = int(width/2), int(height/2)
         cw2, ch2 = int(crop_width/2), int(crop_height/2)
         crop_img = img[mid_y-ch2:mid_y+ch2, mid_x-cw2:mid_x+cw2]
-        # cv2.imwrite('croped.jpg', crop_img)
         return crop_img
 
     def match(self, des1, des2):
@@ -88,8 +86,6 @@ class CVModule():
         matches = matches[:100]
         good = []
 
-        # Tips:弃用，用迭代会引发 ValueError: not enough values to unpack (expected 2, got 1)
-
         for m in matches:
             if len(m) == 2:
                 if m[0].distance < 0.75 * m[1].distance:
@@ -97,6 +93,16 @@ class CVModule():
         return good
 
     def findHomgraphy(self, good, kps1, kps2):
+        """使用RANSAC计算inliner匹配点
+        Args:
+            good ([match]): [两张图象的匹配特征好点]
+            kps1 ([keypoint]): [图像1的关键特侦点]
+            kps2 ([keypoint]): [图像2的关键特侦点]
+
+        Returns:
+            [float]: [置信度]]
+        """
+
         try:
             src_pts = np.float32(
                 [kps1[m.queryIdx].pt for m in good]).reshape(-1, 1, 2)
@@ -106,7 +112,6 @@ class CVModule():
 
             correct_matched_kp = [good[i] for i in range(len(good)) if mask[i]]
             percent = len(correct_matched_kp)/len(mask)
-            # print(f"RANSAC Match length:{percent}")
             return percent
         except Exception as e:
             print(f"findHomgraphy:{e}")
