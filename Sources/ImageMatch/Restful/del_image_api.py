28d374d376c6b4f72bda36d60ce637032a5e4341
NSWell <paris3@163.com>
2021-01-25 15:14:35 +0800

    fix del image

--- Sources/ImageMatch/Restful/del_image_api.py
+++ Sources/ImageMatch/Restful/del_image_api.py
@@ -1,45 +1,39 @@
 from ImageMatch.Restful import *
+from ImageMatch.Cores.image_train import ImageTrain
 
 
 class Image_Del_API(Resource):
     def __init__(self):
-        pass
+        self.args = create_args()
 
     def delete(self):
-        img, args = get_image(CVAlgorithm)
-        img = CVAlgorithm.crop_center(img, dim=dim_800x800)
+        img = get_image(CVAlgorithm, self.args)
+
+        if img is None:
+            return {'msg': 'Image data error'}, 200
+
+        if img.shape != dim_800x800:
+            return {'msg': 'Image size is not enough'}, 200
+
         kps, des = CVAlgorithm.extract_feature(img)
 
         # Init and load search algorithm
-
-        result_table = image_search.search_batch(des)
+        image_search = ImageSearch(annoy_index_db_path)
+        result_table = image_search.search_batch(des, kps)
         image_search.unload()
 
-        # Collection  the id fields
-        result_ids_table = list()
-        [result_ids_table.append(result['id']) for result in result_table]
-        records = ims.search_multiple_record(result_ids_table)
+        if result_table != None:
+            imageTrain = ImageTrain(annoy_index_db_path)
+            ims.delete_siginle_record({'id': result_table['id']})
+            # Loading already dataset and rebuild it!
+            already_dataset = ims.search_all_record()
+            for key in already_dataset:
+                imageTrain.addMarkerDes(
+                    key["_source"]["id"], key["_source"]["des"])
 
-        # Annoy index data found, We also need to match the data one by one
-        for data_index in range(len(result_table)):
-            record = records[data_index]["_source"]
-            data = result_table[data_index]
-            RANSAC_percent = CVAlgorithm.findHomgraphy(
-                data['good'], kps, record['kps'])
-
-            # If it finds any very similar data, it will jump out of the test directly
-            if RANSAC_percent >= 0.5:
-                ims.delete_siginle_record({'id': record['id']})
-
-                # Loading already dataset and rebuild it!
-                already_dataset = ims.search_all_record()
-                for key in already_dataset:
-                    imageTrain.addMarkerDes(
-                        key["_source"]["id"], key["_source"]["des"])
-
-                 # Rebuild index
-                if imageTrain.generateMarkerDB():
-                    return {'msg': success_response}
-                else:
-                    return {'msg': 'eror!'}
+                # Rebuild index
+            if imageTrain.generateMarkerDB():
+                return {'msg': success_response}
+            else:
+                return {'msg': 'eror!'}
         return {'msg': already_img_response}, 202
